cd2c7a901f3d5f18bf95770fa0e479081be57d6b
spgray0426 <spgray@naver.com>
2025-10-11 22:12:20 +0900

    Feat: 공간형 인벤토리에 아이템 카테고리 필터링 기능 추가
    공간형 인벤토리에서 아이템 카테고리(장비, 소모품, 제작재료)별로
    필터링 가능한 버튼을 구현했습니다.
    
    - `UInv_SpatialInventory`에 카테고리 전환 함수(`ShowEquippables`, `ShowConsumables`,
      `ShowCraftables`) 및 그리드 활성화 함수 추가
    - 버튼 클릭 시 Grid와 버튼 상태가 동기화되도록 구현
    - `WBP_Inv_SpatialInventory.uasset`에서 UI에 버튼 추가 및 바인딩 완료
    
    사용자는 버튼을 통해 손쉽게 카테고리를 전환 및 관리할 수 있습니다.

--- Plugins/Inventory/Source/Inventory/Private/Widgets/Inventory/Spatial/Inv_SpatialInventory.cpp
+++ Plugins/Inventory/Source/Inventory/Private/Widgets/Inventory/Spatial/Inv_SpatialInventory.cpp
@@ -2,3 +2,48 @@
 
 
 #include "Widgets/Inventory/Spatial/Inv_SpatialInventory.h"
+
+#include "Components/Button.h"
+#include "Components/WidgetSwitcher.h"
+#include "Widgets/Inventory/Spatial/Inv_InventoryGrid.h"
+
+void UInv_SpatialInventory::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+
+	Button_Equippables->OnClicked.AddDynamic(this, &ThisClass::ShowEquippables);
+	Button_Consumables->OnClicked.AddDynamic(this, &ThisClass::ShowConsumables);
+	Button_Craftables->OnClicked.AddDynamic(this, &ThisClass::ShowCraftables);
+
+	ShowEquippables();
+}
+
+void UInv_SpatialInventory::ShowEquippables()
+{
+	SetActiveGrid(Grid_Equippables, Button_Equippables);
+}
+
+void UInv_SpatialInventory::ShowConsumables()
+{
+	SetActiveGrid(Grid_Consumables, Button_Consumables);
+}
+
+void UInv_SpatialInventory::ShowCraftables()
+{
+	SetActiveGrid(Grid_Craftables, Button_Craftables);
+}
+
+void UInv_SpatialInventory::DisableButtons(UButton* Button)
+{
+	Button_Equippables->SetIsEnabled(true);
+	Button_Consumables->SetIsEnabled(true);
+	Button_Craftables->SetIsEnabled(true);
+	Button->SetIsEnabled(false);
+}
+
+void UInv_SpatialInventory::SetActiveGrid(UInv_InventoryGrid* Grid, UButton* Button)
+{
+	DisableButtons(Button);
+
+	WidgetSwitcher->SetActiveWidget(Grid);
+}
