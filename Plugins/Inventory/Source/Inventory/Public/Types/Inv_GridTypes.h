ea8fe41ca3a5e90c099af07c9d231996000f55b3
spgray0426 <spgray@naver.com>
2025-10-12 20:10:49 +0900

    Feat: 인벤토리 아이템 공간 확인 기능 추가
    인벤토리에서 아이템 추가 가능 여부를 확인하는 `HasRoomForItem` 함수를 구현했습니다.
    
    - `FInv_SlotAvailability` 및 `FInv_SlotAvailabilityResult` 구조체 추가: 슬롯과 공간 정보를 관리
    - `UInv_InventoryBase`와 `UInv_SpatialInventory`에 `HasRoomForItem` 함수 정의 및 구현
    - `UInv_InventoryComponent::TryAddItem`: 아이템 추가 시 공간 확인 로직 통합, 공간 부족 시 처리 로직 추가
    
    이를 통해 아이템 추가 가능 여부를 정확히 판단하고 대응할 수 있습니다.

--- Plugins/Inventory/Source/Inventory/Public/Types/Inv_GridTypes.h
+++ Plugins/Inventory/Source/Inventory/Public/Types/Inv_GridTypes.h
@@ -2,6 +2,8 @@
 
 #include "Inv_GridTypes.generated.h"
 
+class UInv_InventoryItem;
+
 UENUM(BlueprintType)
 enum class EInv_ItemCategory : uint8
 {
@@ -9,4 +11,43 @@ enum class EInv_ItemCategory : uint8
 	Consumable,
 	Craftable,
 	None
+};
+
+USTRUCT()
+struct FInv_SlotAvailability
+{
+	GENERATED_BODY()
+	FInv_SlotAvailability() {};
+	FInv_SlotAvailability(int32 ItemIndex, int32 Room, bool bHasItem) : Index(ItemIndex), AmountToFill(Room), bItemAtIndex(bHasItem) {};
+	
+	// 인벤토리 슬롯의 인덱스
+	int32 Index{INDEX_NONE};
+	
+	// 해당 슬롯에 채울 수 있는 아이템의 수량
+	int32 AmountToFill{0};
+	
+	// 해당 슬롯에 아이템이 있는지 여부
+	bool bItemAtIndex{false};
+};
+
+USTRUCT()
+struct FInv_SlotAvailabilityResult
+{
+	GENERATED_BODY()
+	FInv_SlotAvailabilityResult() {};
+
+	// 검사 중인 인벤토리 아이템에 대한 약한 참조
+	TWeakObjectPtr<UInv_InventoryItem> Item;
+	
+	// 전체 슬롯들에 채울 수 있는 총 아이템 수량
+	int32 TotalRoomToFill{0};
+	
+	// 남은 아이템 수량
+	int32 Remainder{0};
+	
+	// 아이템이 중첩 가능한지 여부
+	bool bStackable{false};
+	
+	// 사용 가능한 슬롯들의 정보 배열
+	TArray<FInv_SlotAvailability> SlotAvailabilities;
 };
