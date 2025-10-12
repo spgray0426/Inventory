122acbafa74d1cd9f2b7f9f3f3258afb60453fe6
spgray0426 <spgray@naver.com>
2025-10-12 20:10:49 +0900

    Feat: 인벤토리 아이템 공간 확인 기능 추가
    인벤토리에서 아이템 추가 가능 여부를 확인하는 `HasRoomForItem` 함수를 구현했습니다.
    
    - `FInv_SlotAvailability` 및 `FInv_SlotAvailabilityResult` 구조체 추가: 슬롯과 공간 정보를 관리
    - `UInv_InventoryBase`와 `UInv_SpatialInventory`에 `HasRoomForItem` 함수 정의 및 구현
    - `UInv_InventoryComponent::TryAddItem`: 아이템 추가 시 공간 확인 로직 통합, 공간 부족 시 처리 로직 추가
    
    이를 통해 아이템 추가 가능 여부를 정확히 판단하고 대응할 수 있습니다.

--- Plugins/Inventory/Source/Inventory/Private/InventoryManagement/Components/Inv_InventoryComponent.cpp
+++ Plugins/Inventory/Source/Inventory/Private/InventoryManagement/Components/Inv_InventoryComponent.cpp
@@ -12,7 +12,13 @@ UInv_InventoryComponent::UInv_InventoryComponent()
 
 void UInv_InventoryComponent::TryAddItem(UInv_ItemComponent* ItemComponent)
 {
-	NoRoomInInventory.Broadcast();
+	FInv_SlotAvailabilityResult Result = InventoryMenu->HasRoomForItem(ItemComponent);
+
+	if (Result.TotalRoomToFill == 0)
+	{
+		NoRoomInInventory.Broadcast();
+		return;
+	}
 }
 
 void UInv_InventoryComponent::ToggleInventoryMenu()
--- Plugins/Inventory/Source/Inventory/Public/Widgets/Inventory/Spatial/Inv_SpatialInventory.h
+++ Plugins/Inventory/Source/Inventory/Public/Widgets/Inventory/Spatial/Inv_SpatialInventory.h
@@ -19,6 +19,8 @@ class INVENTORY_API UInv_SpatialInventory : public UInv_InventoryBase
 
 public:
 	virtual void NativeOnInitialized() override;
+
+	virtual FInv_SlotAvailabilityResult HasRoomForItem(UInv_ItemComponent* ItemComponent) const override;
 private:
 
 	UFUNCTION()
