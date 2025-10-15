debc9f7456fe2c51695c185fd41a3579605cab68
spgray0426 <spgray@naver.com>
2025-10-16 00:16:58 +0900

    Feat: 아이템 프래그먼트 검색 및 그리드 추가 로직 구현
    - `GetFragmentOfTypeWithTag` 템플릿 함수 추가: 태그 기반으로 특정 프래그먼트를 검색
    - `AddItemToIndices` 함수 구현: 공간 확인 결과를 기반으로 아이템을 그리드에 추가
    - `FInv_ImageFragment`로 타입명 변경 및 아이콘 관련 개선
    - `Inv_InventoryItem`에 `GetFragment` 템플릿 함수 추가: 매니페스트에서 프래그먼트 검색
    - `HasRoomForItem` 함수 내 공간 결과값 설정 로직 상향
    
    이를 통해 프래그먼트 시스템을 확장 및 정교화하고, 아이템 추가 과정에서의 데이터 처리를 개선했습니다.

--- Plugins/Inventory/Source/Inventory/Public/Items/Manifest/Inv_ItemManifest.h
+++ Plugins/Inventory/Source/Inventory/Public/Items/Manifest/Inv_ItemManifest.h
@@ -22,10 +22,14 @@ struct INVENTORY_API FInv_ItemManifest
 	UInv_InventoryItem* Manifest(UObject* NewOuter);
 	EInv_ItemCategory GetItemCategory() const { return ItemCategory; }
 	FGameplayTag GetItemType() const { return ItemType; }
+
+	template<typename T> requires std::derived_from<T, FInv_ItemFragment>
+	const T* GetFragmentOfTypeWithTag(const FGameplayTag& FragmentTag) const;
+	
 private:
 
 	UPROPERTY(EditAnywhere, Category = "Inventory", meta = (ExcludeBaseStruct))
-	TArray<TInstancedStruct<FInv_ItemFragment>> InstancedStruct;
+	TArray<TInstancedStruct<FInv_ItemFragment>> Fragments;
 	
 	UPROPERTY(EditAnywhere, Category = "Inventory")
 	EInv_ItemCategory ItemCategory {EInv_ItemCategory::None};
@@ -33,3 +37,17 @@ private:
 	UPROPERTY(EditAnywhere, Category = "Inventory")
 	FGameplayTag ItemType;
 };
+
+template<typename T> requires std::derived_from<T, FInv_ItemFragment>
+const T* FInv_ItemManifest::GetFragmentOfTypeWithTag(const FGameplayTag& FragmentTag) const
+{
+	for (const TInstancedStruct<FInv_ItemFragment>& Fragment : Fragments)
+	{
+		if (const T* FragmentPtr = Fragment.GetPtr<T>())
+		{
+			if (!FragmentPtr->GetFragmentTag().MatchesTagExact(FragmentTag)) continue;
+			return FragmentPtr;
+		}
+	}
+	return nullptr;
+}
