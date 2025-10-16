a3e15b3892736f5ccb093f5460754f4b39dc3261
spgray0426 <spgray@naver.com>
2025-10-16 16:35:55 +0900

    Feat: 프래그먼트 검색 템플릿 함수 추가
    프래그먼트 시스템을 확장하여 새로운 검색 로직을 구현했습니다.
    
    - `GetFragmentOfType` 템플릿 함수 추가: 태그 없이 타입만으로 프래그먼트 검색 가능
    - `GetFragmentOfTypeWithTag`와 동일한 방식으로 반복문을 순회하며 데이터 반환
    
    이를 통해 태그 없이도 프래그먼트 검색이 가능해졌으며, 검색 로직의 유연성과 코드 재사용성을 강화했습니다.

--- Plugins/Inventory/Source/Inventory/Public/Items/Manifest/Inv_ItemManifest.h
+++ Plugins/Inventory/Source/Inventory/Public/Items/Manifest/Inv_ItemManifest.h
@@ -25,7 +25,9 @@ struct INVENTORY_API FInv_ItemManifest
 
 	template<typename T> requires std::derived_from<T, FInv_ItemFragment>
 	const T* GetFragmentOfTypeWithTag(const FGameplayTag& FragmentTag) const;
-	
+
+	template<typename T> requires std::derived_from<T, FInv_ItemFragment>
+	const T* GetFragmentOfType() const;
 private:
 
 	UPROPERTY(EditAnywhere, Category = "Inventory", meta = (ExcludeBaseStruct))
@@ -46,6 +48,19 @@ const T* FInv_ItemManifest::GetFragmentOfTypeWithTag(const FGameplayTag& Fragmen
 		if (const T* FragmentPtr = Fragment.GetPtr<T>())
 		{
 			if (!FragmentPtr->GetFragmentTag().MatchesTagExact(FragmentTag)) continue;
+			return FragmentPtr;
+		}
+	}
+	return nullptr;
+}
+
+template <typename T> requires std::derived_from<T, FInv_ItemFragment>
+const T* FInv_ItemManifest::GetFragmentOfType() const
+{
+	for (const TInstancedStruct<FInv_ItemFragment>& Fragment : Fragments)
+	{
+		if (const T* FragmentPtr = Fragment.GetPtr<T>())
+		{
 			return FragmentPtr;
 		}
 	}
