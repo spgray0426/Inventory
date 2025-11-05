1c5b95aade9b713614be503c32d9a3fb819c0543
spgray0426 <spgray@naver.com>
2025-11-05 22:17:05 +0900

    Feat: 슬롯 클릭 시 아이템 놓기 처리 로직 추가
    슬롯 클릭 이벤트에서 아이템을 놓는 동작과 관련된 초기 로직을 추가했습니다.
    
    - `OnGridSlotClicked` 함수 수정: 호버 아이템의 유효성 및 슬롯 상태 검증 추가
    - 새로운 슬롯이 빈 경우 아이템 놓기 구현을 위한 TODO 주석 추가
    - 클릭된 슬롯의 상태와 입력 이벤트를 기반으로 동작 처리 로직 개선
    
    이를 통해 슬롯 클릭 시 아이템 배치를 처리할 수 있는 기반을 마련했습니다.

--- Plugins/Inventory/Source/Inventory/Private/Widgets/Inventory/Spatial/Inv_InventoryGrid.cpp
+++ Plugins/Inventory/Source/Inventory/Private/Widgets/Inventory/Spatial/Inv_InventoryGrid.cpp
@@ -329,6 +329,20 @@ void UInv_InventoryGrid::OnSlottedItemClicked(int32 GrideIndex, const FPointerEv
 
 void UInv_InventoryGrid::OnGridSlotClicked(int32 GridIndex, const FPointerEvent& MouseEvent)
 {
+	if (!IsValid(HoverItem)) return;
+	if (!GridSlots.IsValidIndex(ItemDropIndex)) return;
+
+	if (CurrentQueryResult.ValidItem.IsValid() && GridSlots.IsValidIndex(CurrentQueryResult.UpperLeftIndex))
+	{
+		OnSlottedItemClicked(CurrentQueryResult.UpperLeftIndex, MouseEvent);
+		return;
+	}
+	
+	auto GridSlot = GridSlots[ItemDropIndex];
+	if (!GridSlot->GetInventoryItem().IsValid())
+	{
+		// TODO: Put item down at this index.
+	}
 }
 
 void UInv_InventoryGrid::OnGridSlotHovered(int32 GridIndex, const FPointerEvent& MouseEvent)
