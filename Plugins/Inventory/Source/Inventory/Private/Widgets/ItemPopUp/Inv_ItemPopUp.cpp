38ca0834724aec03f2faed0f1c24aeb19c6b0c8b
spgray0426 <spgray@naver.com>
2025-11-08 00:17:48 +0900

    feat: 아이템 팝업 위젯 기능 개선 및 코드 가독성 향상
    - 버튼(분리, 버리기, 사용)의 클릭 이벤트 및 슬라이더 값 변경 이벤트 주석 추가
    - 위젯 함수들의 역할 명확화: `CollapseSplitButton`, `CollapseConsumeButton`, `SetSliderParams` 등
    - 델리게이트 및 위젯 컴포넌트의 목적과 연결 구조를 주석으로 보강
    - 팝업의 상호작용 흐름 강화 및 관련 정보를 주석으로 명시
    
    이를 통해 아이템 팝업 관련 코드의 이해도를 높이고 유지보수를 용이하게 개선했습니다.

--- Plugins/Inventory/Source/Inventory/Private/Widgets/ItemPopUp/Inv_ItemPopUp.cpp
+++ Plugins/Inventory/Source/Inventory/Private/Widgets/ItemPopUp/Inv_ItemPopUp.cpp
@@ -12,6 +12,7 @@ void UInv_ItemPopUp::NativeOnInitialized()
 {
     Super::NativeOnInitialized();
 
+    // 각 버튼의 클릭 이벤트와 슬라이더의 값 변경 이벤트를 바인딩
     Button_Split->OnClicked.AddDynamic(this, &ThisClass::SplitButtonClicked);
     Button_Drop->OnClicked.AddDynamic(this, &ThisClass::DropButtonClicked);
     Button_Consume->OnClicked.AddDynamic(this, &ThisClass::ConsumeButtonClicked);
@@ -21,16 +22,19 @@ void UInv_ItemPopUp::NativeOnInitialized()
 void UInv_ItemPopUp::NativeOnMouseLeave(const FPointerEvent& InMouseEvent)
 {
     Super::NativeOnMouseLeave(InMouseEvent);
+    // 마우스가 팝업 영역을 벗어나면 팝업을 화면에서 제거
     RemoveFromParent();
 }
 
 int32 UInv_ItemPopUp::GetSplitAmount() const
 {
+    // 슬라이더 값을 정수로 변환하여 반환
     return FMath::Floor(Slider_Split->GetValue());
 }
 
 void UInv_ItemPopUp::CollapseSplitButton() const
 {
+    // 분리 관련 UI 요소들을 모두 숨김 처리
     Button_Split->SetVisibility(ESlateVisibility::Collapsed);
     Slider_Split->SetVisibility(ESlateVisibility::Collapsed);
     Text_SplitAmount->SetVisibility(ESlateVisibility::Collapsed);
@@ -38,26 +42,30 @@ void UInv_ItemPopUp::CollapseSplitButton() const
 
 void UInv_ItemPopUp::CollapseConsumeButton() const
 {
+    // 사용 버튼을 숨김 처리
     Button_Consume->SetVisibility(ESlateVisibility::Collapsed);
 }
 
 void UInv_ItemPopUp::SetSliderParams(const float Max, const float Value) const
 {
+    // 슬라이더의 최소값, 최대값, 현재값 설정
     Slider_Split->SetMaxValue(Max);
     Slider_Split->SetMinValue(1);
     Slider_Split->SetValue(Value);
-    
-    //Slider_Split의 값이 변경될때 실행되는 함수에서도 변경을 해줘서 실제로는 필요없는 코드
+
+    // Slider_Split의 값이 변경될때 실행되는 함수에서도 변경을 해줘서 실제로는 필요없는 코드
     Text_SplitAmount->SetText(FText::AsNumber(FMath::Floor(Value)));
 }
 
 FVector2D UInv_ItemPopUp::GetBoxSize() const
 {
+    // 루트 사이즈박스의 너비와 높이를 반환
     return FVector2D(SizeBox_Root->GetWidthOverride(), SizeBox_Root->GetHeightOverride());
 }
 
 void UInv_ItemPopUp::SplitButtonClicked()
 {
+    // 분리 델리게이트를 실행하고 성공 시 팝업 제거
     if (OnSplit.ExecuteIfBound(GetSplitAmount(), GridIndex))
     {
         RemoveFromParent();
@@ -66,6 +74,7 @@ void UInv_ItemPopUp::SplitButtonClicked()
 
 void UInv_ItemPopUp::DropButtonClicked()
 {
+    // 버리기 델리게이트를 실행하고 성공 시 팝업 제거
     if (OnDrop.ExecuteIfBound(GridIndex))
     {
         RemoveFromParent();
@@ -74,6 +83,7 @@ void UInv_ItemPopUp::DropButtonClicked()
 
 void UInv_ItemPopUp::ConsumeButtonClicked()
 {
+    // 사용 델리게이트를 실행하고 성공 시 팝업 제거
     if (OnConsume.ExecuteIfBound(GridIndex))
     {
         RemoveFromParent();
@@ -82,5 +92,6 @@ void UInv_ItemPopUp::ConsumeButtonClicked()
 
 void UInv_ItemPopUp::SliderValueChanged(float Value)
 {
+    // 슬라이더 값이 변경되면 텍스트를 정수 형태로 업데이트
     Text_SplitAmount->SetText(FText::AsNumber(FMath::Floor(Value)));
 }
--- Plugins/Inventory/Source/Inventory/Public/Widgets/ItemPopUp/Inv_ItemPopUp.h
+++ Plugins/Inventory/Source/Inventory/Public/Widgets/ItemPopUp/Inv_ItemPopUp.h
@@ -11,8 +11,13 @@ class UTextBlock;
 class USlider;
 class UButton;
 
+/** 분리 버튼 클릭 시 호출되는 델리게이트 (분리 수량, 그리드 인덱스) */
 DECLARE_DYNAMIC_DELEGATE_TwoParams(FPopUpMenuSplit, int32, SplitAmount, int32, Index);
+
+/** 버리기 버튼 클릭 시 호출되는 델리게이트 (그리드 인덱스) */
 DECLARE_DYNAMIC_DELEGATE_OneParam(FPopUpMenuDrop, int32, Index);
+
+/** 사용 버튼 클릭 시 호출되는 델리게이트 (그리드 인덱스) */
 DECLARE_DYNAMIC_DELEGATE_OneParam(FPopUpMenuConsume, int32, Index);
 
 /**
@@ -25,49 +30,77 @@ class INVENTORY_API UInv_ItemPopUp : public UUserWidget
 public:
     virtual void NativeOnInitialized() override;
     virtual void NativeOnMouseLeave(const FPointerEvent& InMouseEvent) override;
-    
+
+    /** 슬라이더로 설정된 분리 수량을 반환 (소수점 버림) */
     int32 GetSplitAmount() const;
+
+    /** 분리 버튼과 슬라이더를 숨김 처리 */
     void CollapseSplitButton() const;
+
+    /** 사용 버튼을 숨김 처리 */
     void CollapseConsumeButton() const;
+
+    /** 슬라이더의 최대값과 현재값을 설정 */
     void SetSliderParams(const float Max, const float Value) const;
+
+    /** 위젯의 크기를 반환 */
     FVector2D GetBoxSize() const;
+
+    /** 그리드 인덱스를 설정 */
     void SetGridIndex(int32 Index) { GridIndex = Index; }
+
+    /** 그리드 인덱스를 반환 */
     int32 GetGridIndex() const { return GridIndex; }
-    
+
+    /** 분리 버튼 클릭 시 실행될 델리게이트 */
     FPopUpMenuSplit OnSplit;
+
+    /** 버리기 버튼 클릭 시 실행될 델리게이트 */
     FPopUpMenuDrop OnDrop;
+
+    /** 사용 버튼 클릭 시 실행될 델리게이트 */
     FPopUpMenuConsume OnConsume;
 private:
-
+    /** 분리 버튼 클릭 시 호출되는 콜백 함수 */
     UFUNCTION()
     void SplitButtonClicked();
 
+    /** 버리기 버튼 클릭 시 호출되는 콜백 함수 */
     UFUNCTION()
     void DropButtonClicked();
 
+    /** 사용 버튼 클릭 시 호출되는 콜백 함수 */
     UFUNCTION()
     void ConsumeButtonClicked();
 
+    /** 슬라이더 값 변경 시 호출되는 콜백 함수 */
     UFUNCTION()
     void SliderValueChanged(float Value);
-    
+
+    /** 분리 버튼 위젯 */
     UPROPERTY(meta = (BindWidget))
     TObjectPtr<UButton> Button_Split;
 
+    /** 버리기 버튼 위젯 */
     UPROPERTY(meta = (BindWidget))
     TObjectPtr<UButton> Button_Drop;
 
+    /** 사용 버튼 위젯 */
     UPROPERTY(meta = (BindWidget))
     TObjectPtr<UButton> Button_Consume;
 
+    /** 분리 수량을 조절하는 슬라이더 위젯 */
     UPROPERTY(meta = (BindWidget))
     TObjectPtr<USlider> Slider_Split;
 
+    /** 분리 수량을 표시하는 텍스트 위젯 */
     UPROPERTY(meta = (BindWidget))
     TObjectPtr<UTextBlock> Text_SplitAmount;
 
+    /** 루트 사이즈박스 위젯 */
     UPROPERTY(meta = (BindWidget))
     TObjectPtr<USizeBox> SizeBox_Root;
 
+    /** 이 팝업이 참조하는 그리드 슬롯의 인덱스 */
     int32 GridIndex{INDEX_NONE};
 };
